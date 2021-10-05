cf568d593ca602eacdf6ed58df51e4e5f4b8c8d5
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-10-05 14:48:26 -0400

    Experts tune-up round 1.

--- website/src/components/Blocks/AddressPanel.js
+++ website/src/components/Blocks/AddressPanel.js
@@ -1,10 +1,48 @@
-import { VStack, useColorMode } from "@chakra-ui/react";
-
+import {
+  IconButton,
+  Text,
+  HStack,
+  VStack,
+  useColorMode,
+} from "@chakra-ui/react";
+import { useMoralis } from "react-moralis";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState, useEffect } from "react";
+import { useExperts } from "../../contexts/expertsContext";
+import { CopyIcon } from "@chakra-ui/icons";
+import EthereumQRPlugin from "ethereum-qr-code";
 const lightModeBG = "linear(to-br,blue.400,red.300,white,red.300,white)";
 const darkModeBG = "linear(to-br,blue.900,grey,red.900,grey,red.900)";
 
 export const AddressPanel = () => {
+  const { Moralis, isAuthenticated } = useMoralis();
   const { colorMode } = useColorMode();
+  const { setActionMode, setDialog } = useExperts();
+  const [copied, setCopied] = useState(false);
+  const [data, setData] = useState("0x0");
+  const user = Moralis.User.current();
+  const ethAddress = user?.attributes.ethAddress;
+  const qr = new EthereumQRPlugin();
+
+  useEffect(() => {
+    if (copied) {
+      setActionMode("recieve");
+      setDialog(
+        "Your wallet address has been copied to the clipboard.  " +
+          "Paste your address as the destination " +
+          "in the market withdraw or send entry, " +
+          "then carefully check the address before sending!  " +
+          "Malware can change your destination address in the clipboard!"
+      );
+      setCopied(false);
+    } else {
+      if (isAuthenticated) {
+        setData(user?.attributes["ethAddress"]);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [copied, isAuthenticated]);
+
   return (
     <VStack
       alignItems="center"
@@ -18,7 +56,18 @@ export const AddressPanel = () => {
       spacing={6}
       bgGradient={colorMode === "light" ? lightModeBG : darkModeBG}
     >
-      Address Panel
+      <Text>Your Address:</Text>
+      <HStack>
+        <Text>{ethAddress}</Text>
+        <CopyToClipboard text={data} onCopy={() => setCopied(true)}>
+          <IconButton
+            disabled={!isAuthenticated}
+            variant="outline"
+            aria-label="Copy Address to Clipboard"
+            icon={<CopyIcon />}
+          />
+        </CopyToClipboard>
+      </HStack>
     </VStack>
   );
 };
