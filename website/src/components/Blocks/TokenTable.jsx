a58c2c256785941bbbe92c114976fe9c9e476469
rumi-art <artyom1209rumi@gmail.com>
2021-10-15 11:38:34 -0700

    add research card component & service hooks

--- website/src/components/Blocks/TokenTable.jsx
+++ website/src/components/Blocks/TokenTable.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   Avatar,
   Box,
@@ -5,24 +6,45 @@ import {
   Text,
   useColorMode,
   VStack,
-} from "@chakra-ui/react";
-import {
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  ModalFooter
 } from "@chakra-ui/react";
 import { usePositions } from "../../hooks/usePositions";
 import { TransactionList } from "./TransactionList";
+import { getDataByCoinID } from '../../hooks/action';
+import Card from '../Research/card';
+import Loader from '../Research/load';
 
 const lightModeBG = "linear(to-br,blue.400,red.300,white,red.300,white)";
 const darkModeBG = "linear(to-br,blue.900,grey,blue.900,grey,blue.900)";
 
 export const TokenTable = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedCoin, setSelectedCoin] = useState(null);
   const { colorMode } = useColorMode();
   const { positions, isLoading, totalValue } = usePositions();
-
+  const getDataApi = getDataByCoinID();
+  const handleClickRow = async p => {
+    onOpen();
+    const data = await getDataApi(p.id);
+    if(data.id) {
+      setSelectedCoin(data)
+    }else {
+      onClose()
+    }
+  }
   return (
     <VStack
       borderWidth={4}
@@ -37,7 +59,7 @@ export const TokenTable = () => {
       <Accordion allowToggle width="100%">
         {!isLoading &&
           positions.map((position) => (
-            <AccordionItem key={position.name} width="100%">
+            <AccordionItem key={position.name} width="100%" onClick={() => handleClickRow(position)}>
               <AccordionButton>
                 <Flex gap={6}>
                   <Box width="50px">
@@ -80,6 +102,19 @@ export const TokenTable = () => {
             </AccordionItem>
           ))}
       </Accordion>
+      <Modal isOpen={isOpen} onClose={onClose} size={'4xl'} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            {
+              selectedCoin ?
+                <Card data={selectedCoin} /> :
+                <Loader />
+            }
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </VStack>
   );
 };
---
+++ website/src/service/types.js
@@ -0,0 +1,4 @@
+
+export const COINGECKO = 'https://api.coingecko.com/api/v3/';
+
+
