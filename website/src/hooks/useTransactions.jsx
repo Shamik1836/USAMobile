ee533c7125874d73b08481048a03fa0791dac0a8
rumi-art <artyom1209rumi@gmail.com>
2021-10-22 07:50:30 -0700

    add try-catch into sendstart & updated Trasactions

--- website/src/hooks/useTransactions.jsx
+++ website/src/hooks/useTransactions.jsx
@@ -11,45 +11,53 @@ export const useTransactions = (props) => {
   const [isLoading, setIsLoading] = useState(1);
 
   useEffect(() => {
-    if (isAuthenticated) {
-      Moralis.Web3API.account
-        .getTransactions({ usePost: true })
-        .then((userTrans) => {
-          console.log("userTrans:", userTrans);
-          let newTxs = userTrans.result.map((Tx) => {
-            const output = { ...Tx };
-            switch (address) {
-              case Tx.from_address:
-                output.counterparty = Tx.to_address;
-                output.amount = -1 * parseFloat(Tx.value);
-                break;
-              case Tx.to_address:
-                output.counterparty = Tx.from_address;
-                output.amount = 1 * parseFloat(Tx.value);
-                break;
-              case undefined:
-                output.counterparty = undefined;
-                output.amount = undefined;
-                break;
-              default:
-                console.debug("Failed address: ", address);
-                console.debug("Failed Tx.from_address:", Tx.from_address);
-                console.debug("Failed Tx.to_address:", Tx.to_address);
-                output.counterparty = null;
-                output.amount = null;
-                break;
-            }
-            return output;
-          });
-          setTxs(newTxs);
-          setIsLoading(0);
-        });
-    } else {
-      setTxs(emptyList);
-      setIsLoading(1);
-    }
+    handleFunc()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [Moralis.Web3, isAuthenticated]);
+
+  const handleFunc = async () => {
+    try {
+      if (isAuthenticated) {
+        const currentUser = Moralis.User.current();
+        const address = currentUser?.attributes.ethAddress;
+        const userTrans = await Moralis.Web3API.account.getTransactions({ address });
+        const userTokenTrans = await Moralis.Web3API.account.getTokenTransfers({ address });
+        let newTxs = userTrans.result.map((Tx) => {
+          const output = { ...Tx };
+          switch (address) {
+            case Tx.from_address:
+              output.counterparty = Tx.to_address;
+              output.amount = -1 * parseFloat(Tx.value);
+              break;
+            case Tx.to_address:
+              output.counterparty = Tx.from_address;
+              output.amount = 1 * parseFloat(Tx.value);
+              break;
+            case undefined:
+              output.counterparty = undefined;
+              output.amount = undefined;
+              break;
+            default:
+              console.debug("Failed address: ", address);
+              console.debug("Failed Tx.from_address:", Tx.from_address);
+              console.debug("Failed Tx.to_address:", Tx.to_address);
+              output.counterparty = null;
+              output.amount = null;
+              break;
+          }
+          return output;
+        });
+        setTxs(newTxs);
+        setIsLoading(0);
+      } else {
+        setTxs(emptyList);
+        setIsLoading(1);
+      }
+    } catch (e) {
+      console.debug('error', e)
+    }
+
+  }
 
   return { Txs, isLoading };
 };
