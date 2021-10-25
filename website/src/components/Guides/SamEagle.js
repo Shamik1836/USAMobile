9ae5536d3cb56a95c27d276cd2b935d933517cb9
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-10-25 16:42:57 -0400

    Touch ups.

--- website/src/components/Guides/SamEagle.js
+++ website/src/components/Guides/SamEagle.js
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 
-import BadgeIcon from "../../media/characters/Sameagle.svg";
+import BadgeIcon from "../../media/characters/SamEagle.svg";
 export const SamEagle = (props) => {
   return (
     <Box
       component="img"
-      sx={{  height: 180, width: 180, transform:"scale(1.75,1.75) translate(32px,35px)"}}
+      sx={{
+        height: 180,
+        width: 180,
+        transform: "scale(1.75,1.75) translate(32px,35px)",
+      }}
       src={BadgeIcon}
       alt="Uncle Sam Eagle"
     />
--- website/src/components/Research/card.jsx
+++ website/src/components/Research/card.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Box, Typography, IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-
-
+import { Box, Typography, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Card({ data, onClose }) {
-
   const { market_data } = data;
   return (
-    <Box className="card" sx={{ py:1, px:6 }}>
-      <Box sx={{ display: 'flex' ,justifyContent: 'space-between', alignItems: 'flex-start', mt: 3.75}}>
-        <Box sx={{display: 'flex' }}>
-          <Box component="img" sx={{ height: 70, width: 70 }} src={data.image.large} />
+    <Box className="card" sx={{ py: 1, px: 6 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          mt: 3.75,
+        }}
+      >
+        <Box sx={{ display: "flex" }}>
+          <Box
+            component="img"
+            sx={{ height: 70, width: 70 }}
+            src={data.image.large}
+          />
           <div className="header-title">
             <Typography>{data.name}</Typography>
             <Typography>Currency</Typography>
@@ -22,20 +30,32 @@ function Card({ data, onClose }) {
             <CloseIcon />
           </IconButton>
         </Box>
-
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', mt: 3.75 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          mt: 3.75,
+        }}
+      >
         <div>
           <Typography className="title">Market Price</Typography>
-          <Typography className="price">${market_data.current_price.usd}</Typography>
-          <Typography className="title">{market_data.current_price.btc} BTC</Typography>
+          <Typography className="price">
+            ${market_data.current_price.usd}
+          </Typography>
+          <Typography className="title">
+            {market_data.current_price.btc} BTC
+          </Typography>
         </div>
         <div>
           <Typography className="title">24H Change</Typography>
           <Typography className="percent">
             {market_data.price_change_percentage_24h}%
           </Typography>
-          <Typography className="i-price">${market_data.price_change_24h}</Typography>
+          <Typography className="i-price">
+            ${market_data.price_change_24h}
+          </Typography>
         </div>
         <div>
           <Typography className="title">7D Change</Typography>
@@ -50,13 +70,22 @@ function Card({ data, onClose }) {
           </Typography>
         </div>
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', mt: 3.75 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          mt: 3.75,
+        }}
+      >
         <div>
           <Typography className="title">Market Cap</Typography>
           <Typography fontSize="14px" className="price">
             ${market_data.market_cap.usd}
           </Typography>
-          <Typography className="title">{market_data.market_cap.btc} BTC</Typography>
+          <Typography className="title">
+            {market_data.market_cap.btc} BTC
+          </Typography>
         </div>
         <div>
           <Typography className="title">24H Volume</Typography>
