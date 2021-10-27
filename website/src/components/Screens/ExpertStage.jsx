b8d8739fee202bf05ad62db12a477f96f333cf1b
Nadeem yousaf <nsourcecodes@gmail.com>
2021-10-27 19:46:50 +0500

    Fixed issue of Long text

--- website/src/components/Screens/ExpertStage.jsx
+++ website/src/components/Screens/ExpertStage.jsx
@@ -50,7 +50,7 @@ export const ExpertStage = () => {
           }}
         >
           <Box sx={{ display: 'flex', flex:1, alignSelf:'center', p: 1.5 }}>
-            <Text>
+            <Text style={{wordBreak:'break-word'}}>
               {dialog}
             </Text>
           </Box>
