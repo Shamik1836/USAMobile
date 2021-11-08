9c2033b6d36f6d968f560d98b02468560cc2e2b1
rumi-art <artyom1209rumi@gmail.com>
2021-11-08 09:14:34 -0800

    updated research card

--- website/src/components/Research/card.jsx
+++ website/src/components/Research/card.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Box, Typography, IconButton } from '@mui/material';
+import { Box, Typography, IconButton, Button } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 function Card({ data, onClose }) {
-  const { market_data } = data;
+  const { market_data, links } = data;
+  const handleClick = (link) => {
+    window.open(link, '_blank');
+  };
+  console.log(data);
   return (
     <Box className="card" sx={{ py: 1, px: 6 }}>
       <Box
@@ -49,20 +53,54 @@ function Card({ data, onClose }) {
           </Typography>
         </Box>
         <Box>
+          <Typography className="title">1H Change</Typography>
+          <Typography
+            className={
+              market_data.price_change_percentage_1h_in_currency.usd > 0
+                ? 'percent'
+                : 'percent negative'
+            }
+          >
+            {market_data.price_change_percentage_1h_in_currency.usd}%
+          </Typography>
+        </Box>
+        <Box>
           <Typography className="title">24H Change</Typography>
-          <Typography className="percent">
+          <Typography
+            className={
+              market_data.price_change_percentage_24h > 0
+                ? 'percent'
+                : 'percent negative'
+            }
+          >
             {market_data.price_change_percentage_24h}%
           </Typography>
-          <Typography className="i-price">
+          <Typography
+            className={
+              market_data.price_change_24h > 0 ? 'i-price' : 'i-price negative'
+            }
+          >
             ${market_data.price_change_24h}
           </Typography>
         </Box>
         <Box>
           <Typography className="title">7D Change</Typography>
-          <Typography className="percent">
+          <Typography
+            className={
+              market_data.price_change_percentage_7d > 0
+                ? 'percent'
+                : 'percent negative'
+            }
+          >
             {market_data.price_change_percentage_7d}%
           </Typography>
-          <Typography className="i-price">
+          <Typography
+            className={
+              market_data.price_change_percentage_7d > 0
+                ? 'i-price'
+                : 'i-price negative'
+            }
+          >
             $
             {(market_data.current_price.usd *
               market_data.price_change_percentage_7d) /
@@ -96,6 +134,26 @@ function Card({ data, onClose }) {
             {market_data.market_cap_change_24h_in_currency.btc} BTC
           </Typography>
         </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+          mt: 3.75,
+        }}
+      >
+        {links.blockchain_site
+          .filter((a) => a)
+          .map((b) => (
+            <Button
+              onClick={() => handleClick(b)}
+              variant="contained"
+              size={links.blockchain_site.length > 5 ? 'small' : 'medium'}
+            >
+              {b.split('/')[2]}
+            </Button>
+          ))}
       </Box>
     </Box>
   );
