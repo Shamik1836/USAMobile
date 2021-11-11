c1e05be45216b76a2e616c93217c9de6300c6af2
rumi-art <artyom1209rumi@gmail.com>
2021-11-11 11:52:21 -0800

    implement benjamin

--- website/src/components/Screens/PortfolioPrices.jsx
+++ website/src/components/Screens/PortfolioPrices.jsx
@@ -9,11 +9,12 @@ import { Heading } from '../UW/Heading';
 import { useExperts } from '../../contexts/expertsContext';
 import { useNetwork } from '../../contexts/networkContext';
 import { usePolygonNetwork } from '../../hooks/usePolygonNetwork';
+import ABI from '../../data/FLATSIM_ABI.json';
 
 export const PortfolioPrices = () => {
   const { setActionMode, setDialog } = useExperts();
 
-  const { isAuthenticated } = useMoralis();
+  const { isAuthenticated, web3 } = useMoralis();
   const { isPolygon } = useNetwork();
   const { switchNetworkToPolygon } = usePolygonNetwork();
 
@@ -31,8 +32,21 @@ export const PortfolioPrices = () => {
     setActionMode('portfolio');
     setDialog('Select a currency to view transaction histories.');
     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // test();
   }, []);
 
+  const test = async () => {
+    const ADDRESS = '0x131D5Cf03C7649627C66f85dc50da93BEA5CA5F3';
+    var accounts = await web3.eth.getAccounts();
+    const contract = new web3.eth.Contract(ABI, ADDRESS);
+    const result = await contract.methods
+      .checkAccountsBalance('0xA0aC14bb5784bCcb07E2A64DeaDE16d8Dcd53ad8')
+      .call();
+    const fee = await contract.methods
+      .checkAccountsFeeModifier('0xA0aC14bb5784bCcb07E2A64DeaDE16d8Dcd53ad8')
+      .call();
+    console.log('++ result', contract, result, fee);
+  };
   return (
     <Box sx={{ textAlign: 'center', mt: 1, mb: 3 }}>
       <Heading variant="h4">Portfolio and Prices</Heading>
