4ae041dbe34099c4a7cdc0f528b5969ef8f813b3
Thomas Nourse <thomas.nourse.dev@gmail.com>
2021-11-15 01:55:59 -0600

    filter out zero value

--- website/src/hooks/usePositions.jsx
+++ website/src/hooks/usePositions.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useMoralis } from 'react-moralis';
 import { useNetwork } from '../contexts/networkContext';
-import coinGeckoList from '../data/coinGeckoTokenList.json';
+import geckoCoinIds from '../data/geckoCoinIds.json';
 
 const emptyList = [];
 const geckoHead =
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=';
-const geckoTail = '&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 export const usePositions = () => {
   const { isInitialized, isAuthenticated, Moralis } = useMoralis();
@@ -21,36 +20,38 @@ export const usePositions = () => {
       Moralis.Web3.getAllERC20({ chain: networkName })
         .then((allPositions) => {
           const ids = allPositions
-            .map((token) => coinGeckoList[token.symbol.toLowerCase()]?.id)
+            .map((token) => geckoCoinIds[token.symbol.toLowerCase()])
             .filter((id) => Boolean(id))
             .join(',');
 
-          fetch(`${geckoHead}?vs_currency=usd&ids=${ids}` + geckoTail)
+          fetch(`${geckoHead}?vs_currency=usd&ids=${ids}`)
             .then((response) => response.json())
             .then((data) => {
               const objects = {};
               data.forEach((d) => (objects[d.symbol.toUpperCase()] = d));
 
               let runningTotal = 0;
-              const newList = allPositions.map((token) => {
-                const { symbol, balance, decimals } = token;
-                const {
-                  id,
-                  image,
-                  current_price: price,
-                } = objects[symbol] || {};
-                const tokens = balance ? balance / 10 ** decimals : 0;
-                const value = price ? tokens * price : 0;
-                runningTotal += value;
-                return {
-                  ...token,
-                  id,
-                  image,
-                  price,
-                  tokens,
-                  value,
-                };
-              });
+              const newList = allPositions
+                .map((token) => {
+                  const { symbol, balance, decimals } = token;
+                  const {
+                    id,
+                    image,
+                    current_price: price,
+                  } = objects[symbol] || {};
+                  const tokens = balance ? balance / 10 ** decimals : 0;
+                  const value = price ? tokens * price : 0;
+                  runningTotal += value;
+                  return {
+                    ...token,
+                    id,
+                    image,
+                    price,
+                    tokens,
+                    value,
+                  };
+                })
+                .filter((token) => token.value);
               setPositions(newList);
               setTotalValue(runningTotal);
               setIsLoading(0);
